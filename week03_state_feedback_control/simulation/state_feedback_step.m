798408aa2053c19a8a46f37c213d161be6f0f1d6
Eniyavan T <eniyavan.thangabharathi@gmail.com>
2026-01-22 00:35:03 +0530

    Update state_feedback_step.m

--- week03_state_feedback_control/simulation/state_feedback_step.m
+++ week03_state_feedback_control/simulation/state_feedback_step.m
@@ -1,1 +1,17 @@
+% Load state-space model
+run('../model/msd_state_space.m');
+
+% State feedback gain (simple)
+K = [5 2];   % [position gain  velocity gain]
+
+% Closed-loop system
+A_cl = A - B*K;
+sys_cl = ss(A_cl, B, C, D);
+
+% Step response comparison
+figure;
+step(sys_ss, sys_cl);
+grid on;
+legend('Open-loop', 'With State Feedback');
+title('Effect of State Feedback on MSD System');
 
